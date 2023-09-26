1f762d3f473710ad2a54666994204c5f9d19436e
carlosezmz <carlos.ezmz@gmail.com>
2023-09-26 17:51:07 -0400

    flask app

--- Week-05-Dashboards/flask/app.py
+++ Week-05-Dashboards/flask/app.py
@@ -1,53 +1,129 @@
-# Importing required functions
-from flask import Flask, render_template
+import flask
+import os
+import pickle
 import pandas as pd
-import numpy as np
-from matplotlib import pyplot as plt
-import io
-import base64
 
-# Flask constructor
-app = Flask(__name__)
+import json
+import plotly
+import plotly.express as px
 
-df = pd.read_csv("cats_vs_dogs.csv")
 
-# Function to create and save Matplotlib figures
-def create_and_save_figures():
-    x = df['state']
-    y = df['n_pet_households']
-    fig, ax = plt.subplots(figsize =(16, 9))
-    ax.barh(x, y)
-    plt.xlabel("Number of Pet Households")
-    plt.ylabel("State")
-    plt.title('States and their Pet Households')
-    graph1_filename = 'static/graph1.png'
-    plt.savefig(graph1_filename)
-    x = df['dog_population']
-    y = df['cat_population']
-    fig, ax = plt.subplots(figsize =(16, 9))
-    plt.scatter(x, y)
-    plt.xlabel("Dog Population")
-    plt.ylabel("Cat Population")
-    plt.title('Dog and Cat Population Comparison')
-    graph2_filename = 'static/graph2.png'
-    plt.savefig(graph2_filename)
 
-    return graph1_filename, graph2_filename
+app = flask.Flask(__name__, template_folder='templates')
 
-# Route to display the graphs
-@app.route('/')
-def display_graphs():
-    # Create and save Matplotlib figures
-    graph1_filename, graph2_filename = create_and_save_figures()
-    
-    # Read the saved images as binary data
-    with open(graph1_filename, 'rb') as graph1_file:
-        graph1_data = base64.b64encode(graph1_file.read()).decode('utf-8')
+path_to_vectorizer = 'models/vectorizer.pkl'
+path_to_text_classifier = 'models/text-classifier.pkl'
+path_to_image_classifier = 'models/image-classifier.pkl'
 
-    with open(graph2_filename, 'rb') as graph2_file:
-        graph2_data = base64.b64encode(graph2_file.read()).decode('utf-8')
+with open(path_to_vectorizer, 'rb') as f:
+    vectorizer = pickle.load(f)
 
-    return render_template('index.html', graph1_data=graph1_data, graph2_data=graph2_data)
+with open(path_to_text_classifier, 'rb') as f:
+    model = pickle.load(f)
+
+with open(path_to_image_classifier, 'rb') as f:
+    image_classifier = pickle.load(f)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        # Just render the initial form, to get input
+        my_new_var = "Hello Everyone!"
+        return(flask.render_template('index.html', 
+            my_python_variable=my_new_var) )
+
+
+    if flask.request.method == 'POST':
+        # Get the input from the user.
+        user_input_text = flask.request.form['user_input_text']
+        
+        # Turn the text into numbers using our vectorizer
+        X = vectorizer.transform([user_input_text])
+        
+        # Make a prediction 
+        predictions = model.predict(X)
+        
+        # Get the first and only value of the prediction.
+        prediction = predictions[0]
+
+        # Get the predicted probabs
+        predicted_probas = model.predict_proba(X)
+
+        # Get the value of the first, and only, predicted proba.
+        predicted_proba = predicted_probas[0]
+
+        # The first element in the predicted probabs is % democrat
+        precent_democrat = predicted_proba[0]
+
+        # The second elemnt in predicted probas is % republican
+        precent_republican = predicted_proba[1]
+
+
+        return flask.render_template('index.html', 
+            input_text=user_input_text,
+            result=prediction,
+            precent_democrat=precent_democrat,
+            precent_republican=precent_republican,
+            new_variable='hello variables')
+
+
+
+
+@app.route('/input_values/', methods=['GET', 'POST'])
+def input_values():
+    if flask.request.method == 'GET':
+        # Just render the initial form, to get input
+        return(flask.render_template('input_values.html'))
+
+    if flask.request.method == 'POST':
+        # Get the input from the user.
+        var_one = flask.request.form['input_variable_one']
+        var_two = flask.request.form['another-input-variable']
+        var_three = flask.request.form['third-input-variable']
+
+        list_of_inputs = [var_one, var_two, var_three]
+
+        return(flask.render_template('input_values.html', 
+            returned_var_one=var_one,
+            returned_var_two=var_two,
+            returned_var_three=var_three,
+            returned_list=list_of_inputs))
+
+    return(flask.render_template('input_values.html'))
+
+
+@app.route('/images/')
+def images():
+    return flask.render_template('images.html')
+
+
+@app.route('/bootstrap/')
+def bootstrap():
+    return flask.render_template('bootstrap.html')
+
+
+
+@app.route('/charts/', methods=['GET', 'POST'])
+def charts():
+    df = pd.DataFrame({
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    })
+
+    fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    header="Fruit in North America"
+    description = """
+    A academic study of the number of apples, oranges and bananas in the cities of
+    San Francisco and Montreal would probably not come up with this chart.
+    """
+    return flask.render_template('charts.html', graphJSON=graphJSON, header=header,description=description)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
